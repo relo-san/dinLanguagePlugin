29e3af749943efa36bc9df962dc609c435fd5a10
Nicolay N.Zyk <relo.san.pub@gmail.com>
2010-09-22 23:54:42 +0300

    Change location for split dictionary from key.

--- lib/i18n/dinI18n.class.php
+++ lib/i18n/dinI18n.class.php
@@ -46,6 +46,31 @@ class dinI18n extends sfI18N
 
     } // dinI18n::getMessageFormat()
 
+
+    /**
+     * Get translation for key
+     * 
+     * @param   string  $source     Source key
+     * @param   array   $args       Arguments for translation [optional]
+     * @param   string  $catalogue  Catalogue name [optional]
+     * @return  string  Translated string
+     */
+    public function __( $source, $args = array(), $catalogue = null )
+    {
+
+        $parsed = explode( '.', $source, 2 );
+        if ( !isset( $parsed[1] ) )
+        {
+            array_unshift( $parsed, 'messages' );
+        }
+        if ( $parsed[0] === 'null' )
+        {
+            return $parsed[1];
+        }
+        return $this->getMessageFormat()->format( $parsed[1], $args, $parsed[0] );
+
+    } // dinI18n::__()
+
 } // dinI18n
 
 //EOF
